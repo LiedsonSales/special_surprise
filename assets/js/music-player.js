9a6e61427e783c10c821206c3c4ab237114ee805
Liedson Sales <liedson.r.sales@gmail.com>
2025-07-28 19:47:59 -0300

    old code again

--- assets/js/music-player.js
+++ assets/js/music-player.js
@@ -4,39 +4,38 @@ let playbtn = document.getElementById('playbtn');
 let playMusic = document.getElementById('fa-play');
 
 var playPause = function() {
-    if (player.paused) {
-        player.play().then(() => {
-            playbtn.classList.remove('fa-play');
-            playbtn.classList.add('fa-pause');
-        }).catch((err) => {
-            console.error("Erro ao tocar áudio:", err);
-        });
-    } else {
-        player.pause();
-        playbtn.classList.remove('fa-pause');
-        playbtn.classList.add('fa-play');
-    }
-
-    // alert('tocou');
     // if (player.paused) {
-    //     player.play()
+    //     player.play().then(() => {
+    //         playbtn.classList.remove('fa-play');
+    //         playbtn.classList.add('fa-pause');
+    //     }).catch((err) => {
+    //         console.error("Erro ao tocar áudio:", err);
+    //     });
     // } else {
-    //     player.pause()
+    //     player.pause();
+    //     playbtn.classList.remove('fa-pause');
+    //     playbtn.classList.add('fa-play');
     // }
+
+    if (player.paused) {
+        player.play()
+    } else {
+        player.pause()
+    }
 }
 
 playbtn.addEventListener('click', playPause);
 
 
-// player.onplay = function() {
-//     playbtn.classList.remove('fa-play');
-//     playbtn.classList.add('fa-pause');
-// }
+player.onplay = function() {
+    playbtn.classList.remove('fa-play');
+    playbtn.classList.add('fa-pause');
+}
 
-// player.onpause = function() {
-//     playbtn.classList.add('fa-play');
-//     playbtn.classList.remove('fa-pause');
-// }
+player.onpause = function() {
+    playbtn.classList.add('fa-play');
+    playbtn.classList.remove('fa-pause');
+}
 
 player.ontimeupdate = function() {
     let ct = player.currentTime;
